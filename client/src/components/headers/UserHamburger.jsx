5a2efd4e3a6a0f8bd83436bebd46c6038ab1dfff
hamdyyemadd <hamdyyemad@aol.com>
2024-01-05 17:17:56 +0200

    adding translation
    adding translation

--- client/src/components/headers/UserHamburger.jsx
+++ client/src/components/headers/UserHamburger.jsx
@@ -48,7 +48,7 @@ export default function UserHamburger({
                   to="/profile"
                   className="transition-colors duration-200 block px-4 py-2 text-normal text-gray-900 rounded hover:bg-purple-500 hover:text-white"
                 >
-                  Profile
+                  {t("homepage.profile")}
                 </Link>
                 <div className="py-2">
                   <hr></hr>
@@ -57,7 +57,7 @@ export default function UserHamburger({
                   onClick={handleLogout}
                   className="transition-colors text-left w-full duration-200 block px-4 py-2 text-normal text-gray-900 rounded hover:bg-purple-500 hover:text-white"
                 >
-                  Logout
+                  {t("homepage.loguot")}
                 </button>
               </div>
             )}
