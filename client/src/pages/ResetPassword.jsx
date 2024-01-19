728b8e3ec4b5bbbbfb5c82969df2b2e38900fa1f
hamdyyemadd <hamdyyemad@aol.com>
2024-01-19 15:02:15 +0200

    translating reset password
    translating reset password

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -2,9 +2,13 @@ import { Button, Card, Typography } from "@material-tailwind/react";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import CustomSpinner from "../components/CustomSpinner";
+import i18n from "../i18n";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 export default function ResetPassword() {
   const [email, setEmail] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const { t } = useTranslation();
   const handleResetPassword = async (e) => {
     e.preventDefault();
     if (!email.trim()) {
@@ -47,10 +51,10 @@ export default function ResetPassword() {
       {isLoading && <CustomSpinner />}
       {/* <div className="max-w-lg mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300"> */}
       <Typography variant="h3" color="blue-gray" className="dark:text-white">
-        Reset password
+        {t("reset_password.reset")}
       </Typography>
       <Typography color="gray" className="mt-1 dark:text-white font-normal">
-        Fill up the form to reset the password
+        {t("reset_password.fill")}
       </Typography>
 
       <form
@@ -60,14 +64,14 @@ export default function ResetPassword() {
         <div className="flex flex-col space-y-5">
           <label htmlFor="email">
             <p className="font-medium text-slate-700 pb-2 dark:text-white">
-              Email address
+              {t("reset_password.email")}
             </p>
             <input
               id="email"
               name="email"
               type="email"
               className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
-              placeholder="Enter email address"
+              placeholder={t("reset_password.enter")}
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
@@ -89,16 +93,17 @@ export default function ResetPassword() {
               >
                 <path d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z" />
               </svg>
-              <p className="px-2">Reset password</p>
+              <p className="px-2">{t("reset_password.reset")}</p>
             </span>
           </Button>
           <p className="text-center dark:text-white">
-            Not registered yet?{" "}
-            <a
-              href="#"
+            {t("reset_password.not_registered")}
+            {i18n.dir() === "rtl" ? "؟" : "?"}{" "}
+            <Link
+              to="/register"
               className="text-indigo-600 font-medium inline-flex space-x-1 items-center"
             >
-              <span>Register now </span>
+              <span>{t("reset_password.register")} </span>
               <span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -110,7 +115,7 @@ export default function ResetPassword() {
                   <path d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                 </svg>
               </span>
-            </a>
+            </Link>
           </p>
         </div>
       </form>
